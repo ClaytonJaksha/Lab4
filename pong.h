cbdc6c04377bcada7860d11d77f0582266a96256
ClaytonJaksha <c16clayton.jaksha@usafa.edu>
2014-10-22 21:11:56 -0600

    Update pong.h

--- pong.h
+++ pong.h
@@ -5,15 +5,32 @@
  * Description: Header file for pong. Declares functions and defines the ball structure.
  */
 
-#ifndef _PONG_H
-#define _PONG_H
-#define TRUE 1 //will be used for our collision flag
-#define FALSE 0 //same as above
-#define SCREEN_WIDTH 10 //screen parameters inserted here
-#define SCREEN_HEIGHT 8
-#define INIT_VELOC_X 1
-#define INIT_VELOC_Y 1
-#define PONG_WALL 11
+#ifndef 	_PONG_H
+#define 	_PONG_H
+
+#define 	TRUE 			1 //will be used for our collision flag
+#define 	FALSE 			0 //same as above
+#define 	SCREEN_WIDTH 	10 //screen parameters inserted here
+#define 	SCREEN_HEIGHT 	8
+#define 	INIT_VELOC_X 	1
+#define 	INIT_VELOC_Y 	1
+#define 	PONG_WALL 		11
+#define     TRUE            1
+#define     FALSE           0
+#define     UP_BUTTON       (P2IN & BIT5)
+#define     DOWN_BUTTON     (P2IN & BIT4)
+#define     AUX_BUTTON      (P2IN & BIT3)
+#define     LEFT_BUTTON     (P2IN & BIT2)
+#define     RIGHT_BUTTON    (P2IN & BIT1)
+#define		MIN_DELAY		50000
+#define		MAX_DELAY		150000
+
+//declare the functions from the Nokia display assembly file
+extern void init();
+extern void initNokia();
+extern void clearDisplay();
+extern void drawBlock(unsigned char row, unsigned char col);
+extern void drawLine(unsigned char row, unsigned char col);
 
 //used to build our velocity and position structure types
 typedef struct {
@@ -28,14 +45,15 @@ typedef struct {
     unsigned char radius;
 } ball_t;
 
+//paddle structure defined here
 typedef struct {
     int y;
     int top;
     int bottom;
     int radius;
 } paddle_t;
-//here we declare all of our wonderful functions
 
+//here we declare all of our wonderful functions
 ball_t createBall(int xPos, int yPos, int xVel, int yVel, unsigned char radius);
 
 ball_t moveBall(ball_t ballToMove, paddle_t paddlePos);
@@ -49,5 +67,11 @@ unsigned char collisionDetectBottom(ball_t ball);
 unsigned char collisionDetectLeft(ball_t ball, paddle_t paddle);
 
 unsigned char collisionDetectRight(ball_t ball);
+
+void drawGo(unsigned long i, unsigned char j);
+
+void drawArmy(unsigned long i, unsigned char j);
+
+void drawArmyA(unsigned char j);
 
 #endif
